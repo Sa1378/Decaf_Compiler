4fc3324f353453376d9aaf2c5f3c06ab4abb20c8
mo-ma <m.mahdavi79@gmail.com>
2020-07-31 21:18:44 +0430

    somethings for print

--- src/Model/StmtBlock.java
+++ src/Model/StmtBlock.java
@@ -1,6 +1,8 @@
 package Model;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class StmtBlock extends Stmt{
     ArrayList<VariableDecl> varDecls;
@@ -15,10 +17,12 @@ public class StmtBlock extends Stmt{
         cgen.addScope();
         for (VariableDecl varDecl: varDecls){
             varDecl.location = cgen.stackOffset; //TODO init variable with null values?
+            varDecl.varType = VarType.LOCAL;
             cgen.stackOffset -= 4;
             cgen.topScope().put(varDecl.identifier,varDecl);
         }
-        for (Stmt stmt:stmts){
+        Collections.reverse(stmts);
+        for (Stmt stmt: stmts){
             stmt.cgen(cgen);
         }
         cgen.popScope();
